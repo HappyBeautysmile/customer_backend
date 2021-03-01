3e6042612a92863b28561d8945a08727d6072fc8
DarkBear0121 <darkbear0112@gmail.com>
2021-03-02 00:34:33 +0300

    update

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const app = express();
+const server = require("http").Server(app);
+const path = require("path");
+const Config = require("./db")
+
+app.use(cors());
+app.use("*",(req,res,next)=>{
+  let expires = new Date(new Date().valueOf() + 30 * 24 * 60 * 60 * 1000);
+  res.cookie('cookie1', 'value1', { sameSite: 'lax',httpOnly : true ,expires :expires ,path : "/" });   next(); });
+app.use(express.static('./client'));
+app.use(express.static('./client/stellar_admin_frontend_build'));
+app.use(bodyParser.json({limit: "15360mb", type:'application/json'}));
+app.use(bodyParser.urlencoded({ extended: true }));
+  
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'client/customer_frontend_build/index.html'));  
+});
+
+server.listen(Config.PORT, () => {
+  console.log(`Started server on => http://localhost:${Config.PORT}`);
+});
